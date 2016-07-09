3f1a69f6e42718af0dc68ad95aeb6652a5c64923
harpreetsingh91 <harpreet.singhgt@gmail.com>
2016-07-09 13:35:11 -0700

    Flask server implemented

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template,send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def run_game():
+    return render_template("index.html")
+
+
+@app.route('/js/<path:path>') #function defined to serve static files using flask server
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/assets/<path:path>') #function defined to serve static files using flask server
+def send_assets(path):
+    return send_from_directory('assets', path)
+
+if __name__ =='__main__':
+	app.run(host = "localhost", port = 8080, debug = True)
--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-var game = new Phaser.Game(640,360, Phaser.AUTO); //.AUTO ensures sytem falls back to canvas incase webGL is not supported
-
 var GameState = {
+  //load game assets
   preload: function(){
-
+    this.load.image('background', 'assets/images/background.png');
   },
   create: function(){
-
+    this.background = this.game.add.sprite(0,0,'background');
   },
   update: function(){
 
   }
 };
+
+//initiate phaser
+var game = new Phaser.Game(640,360, Phaser.AUTO); //.AUTO ensures sytem falls back to canvas incase webGL is not supported
+
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
